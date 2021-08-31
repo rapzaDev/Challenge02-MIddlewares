4d088a0789be9433b1948ebc86d4471653dc9fc7
rapzaDev <rapzadev@gmail.com>
2021-08-31 15:10:51 -0300

    Challenge 02 - First Commit

--- src/index.js
+++ src/index.js
@@ -10,19 +10,60 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find( (user) => user.username === username );
+  if (!user) return response.status(404);
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro) next();
+  else {
+    const userTodos = user.todos;
+
+    if ( userTodos.length < 10 ) next();
+    else return response.status(403);
+  }
+
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const { id } = request.params;
+
+  const user = users.find( (user) => user.username === username );
+  if (!user) return response.status(404);
+
+  const checkUUID = validate(id);
+  if (!checkUUID) return response.status(400);
+
+  const todo = user.todos.find( (todo) => todo.id === id );
+  if (!todo) return response.status(404);
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find( (user) => user.id === id );
+  if (!user) return response.status(404);
+  
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
